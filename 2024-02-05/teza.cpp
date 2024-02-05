88a149457c470fac2ae04791d707ed490176fb8c
Maria <maria.maiorescu@gmail.com>
2024-02-05 12:01:22 +0200

    ora online

---
+++ 2024-02-05/teza.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+bool litereMari(char s[]) {
+    for (int i = 0; i < strlen(s); i++) {
+        if ('A' < s[i] || s[i] > 'Z') {
+            return false;
+        }
+    }
+   return true;
+}
+
+int main() {
+    char s[256], *p, sep[] = " ,.?!";
+    int n, cuvinte, propozitii = 0;
+
+    ifstream f("date.in");
+    f >> n;
+    f.get();
+
+    for (int i = 0; i < n; i++) {
+        f.getline(s, 256);
+        cout << s << endl;
+
+        p = strtok(s, sep);
+        cuvinte = 0;
+        while (p != NULL) {
+            if(litereMari(p) == 1) {
+                cuvinte++;
+            }
+            p = strtok(NULL, sep);
+        }
+        if(cuvinte == 1) {
+            propozitii++;
+        }
+    }
+    cout << "linii cu 1 cuv doar cu litere mari: " << propozitii << endl;
+    
+    return 0;
+}
